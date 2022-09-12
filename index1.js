10b6b044be6a3519341ffbd3838f863b379a6ee2
shklyarikilya <roflerok535@gmail.com>
2022-09-12 21:57:31 +0300

    hm3

---
+++ index1.js
@@ -0,0 +1,10 @@
+function randomInRange(min, max) {
+    const numbers = {};
+    for (let i = 0; i < 10000000; i++) {
+        const num = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (numbers[num]) { numbers[num] += 1; }
+        else { numbers[num] = 1; }
+    }
+    return numbers;
+}
+console.log(randomInRange(3, 7));
---
+++ index2.js
@@ -0,0 +1,5 @@
+function towerOfHanoi(n)
+{
+return Math.pow(2,n)-1;
+}
+console.log(towerOfHanoi(4))
